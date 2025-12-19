916eaf2957daf958bb0dbae0973fcf8aedcf781e
Mayuri Ghorude <mayurighorude123@gmail.com>
2025-12-19 19:13:29 +0530

    fix Avien mysql ssl connection

--- backend-node/config/db.js
+++ backend-node/config/db.js
@@ -1,29 +1,17 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitforConnection: true,
-  connectionLimit:10,
-  queueLimit:0,
-
-  // 🔴 AIVEN + RENDER KE LIYE YE MOST IMPORTANT HAI
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
   ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-db.getConnection((err, connection) => {
-  if (err) {
-    console.error("❌ Aiven MySQL connection failed:", err);
-    
-  } else {
-    console.log("✅ Aiven MySQL Connected Successfully");
-    connection.release();
+    rejectUnauthorized: false
   }
 });
 
-module.exports = db;
+module.exports = pool.promise();
